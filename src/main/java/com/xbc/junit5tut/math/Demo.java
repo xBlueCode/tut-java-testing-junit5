fb4f930b5761eb118adf88957da510e679e24d5e
xBlueCode <xbluecode@localhost.localdomain>
2018-05-07 00:08:38 +0300

    Update math package

--- src/main/java/com/xbc/junit5tut/math/Demo.java
+++ src/main/java/com/xbc/junit5tut/math/Demo.java
@@ -1,4 +1,16 @@
 package com.xbc.junit5tut.math;
 
 public class Demo {
+    public static void main(String[] args) {
+
+        int a = 10 ;
+        int b = 0 ;
+
+        System.out.println(a +"+"+ b +" = "+ EasyMath.add(a,b));
+        System.out.println(a +"-"+ b +" = "+ EasyMath.substract(a,b));
+        System.out.println(a +"/"+ b +" = "+ EasyMath.divide(a,b));
+        System.out.println(a +" is "+ EasyMath.getSign(a));
+        System.out.println(b +" is "+ EasyMath.getSign(b));
+        System.out.println("Max Number is "+ EasyMath.max(a,b));
+    }
 }
--- src/main/java/com/xbc/junit5tut/math/EasyMath.java
+++ src/main/java/com/xbc/junit5tut/math/EasyMath.java
@@ -1,4 +1,35 @@
 package com.xbc.junit5tut.math;
 
 public class EasyMath {
+
+    public static int add(int n1, int n2) {
+        return n1 + n2;
+    }
+
+    public static int substract(int n1, int n2) {
+        return n1 - n2;
+    }
+
+    public static int divide(int n1, int n2) throws IllegalArgumentException {
+        if (n2 == 0) throw new IllegalArgumentException("Devisor == 0 !!!");
+        return n1 / n2;
+    }
+
+    public static String getSign(int n) {
+        if (n == 0) return "Null";
+        if (n > 0) return "Positive";
+        else return "Negative";
+    }
+
+    public static int max(int n1, int n2) {
+        return n1>=n2 ? n1:n2 ;
+    }
+
+    public static int sum(int ... numbers) {
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++)
+            sum += numbers[i];
+
+        return sum;
+    }
 }
